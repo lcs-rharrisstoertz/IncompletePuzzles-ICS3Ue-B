ddb7ebce48fe13e20e29da563b518de5ba4446d6
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-01-25 12:18:05 -0500

    Finished the "shifty sums" puzzle. Passes all tests successfully.

--- ShiftySums/main.swift
+++ ShiftySums/main.swift
@@ -19,15 +19,27 @@ print("===========")
 let n = Int(readLine()!)!
 
 // How many times should we shift?
-let k = 3
+let k = Int(readLine()!)!
 
 // PROCESS
 
-// Shift the given number "k" number of times
-var output = n
-output += n * 10
-output += n * 10 * 10
-output += n * 10 * 10 * 10
 
-// OUTPUT
-print(output)
+func shift (n: Int, k: Int) -> Int {
+    var output = n
+    if k > 0 { /*the test tried to enter 0, but I can't have a range from 1 to 0. So I added the stipulation that that code should only run if K is greater than 0. If it's 0, then it just returns the original number, n*/
+        for i in 1...k {
+            print("i is \(i)")
+            var toAdd = n
+            for _ in 1...i {
+                toAdd *= 10
+            }
+            output+=toAdd
+        }
+    }
+return output
+}
+
+//OUTPUT
+let shiftedNumber = shift(n:n, k:k)
+print(shiftedNumber)
+
