4768e80b06691b6d5fb1a583552900a15a7d1cbe
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-02 14:42:37 -0500

    finished making a collectInput extension for double

--- Extension.swift
+++ Extension.swift
@@ -74,34 +74,34 @@ extension Double {
                 continue
             }
             
-            // Convert to an integer
-            guard let givenInteger = Int(givenInput) else {
+            // Convert to an double
+            guard let givenDouble = Double(givenInput) else {
                 continue
             }
             
-            // If a lowest value for the integer was specified...
+            // If a lowest value for the double was specified...
             if let minimumValue = minimum {
                 
-                // ... then check that the given integer is greater than or equal to the lowest desired value.
-                guard givenInteger >= minimumValue else {
+                // ... then check that the given double is greater than or equal to the lowest desired value.
+                guard givenDouble >= minimumValue else {
                     continue
                 }
                 
             }
             
-            // If an highest possible value for the integer was specified...
+            // If an highest possible value for the double was specified...
             if let maximumValue = maximum {
                 
-                // ... then check that the given integer is less than or equal to the highest desired value.
-                guard givenInteger <= maximumValue else {
+                // ... then check that the given double is less than or equal to the highest desired value.
+                guard givenDouble <= maximumValue else {
                     continue
                 }
                 
                 
             }
             
-            // If we've made it past all the checks, the input is an integer in the desired range of values, so, return it
-            return givenInteger
+            // If we've made it past all the checks, the input is an double in the desired range of values, so, return it
+            return givenDouble
             
         }
         
