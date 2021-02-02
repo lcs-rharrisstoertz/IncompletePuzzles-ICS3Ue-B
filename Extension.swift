e7f7e6db2cf2c16ef877d1fc3d4799dd67ea206a
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-02 14:46:42 -0500

    added string extension

--- Extension.swift
+++ Extension.swift
@@ -108,3 +108,38 @@ extension Double {
     }
     
 }
+
+extension String {
+    
+    static func collectInput(withPrompt prompt: String, acceptableValues: [String]?) -> String {
+        
+        // Loop until a valid value is provided
+        while true {
+            
+            // Prompt the user
+            print(prompt, terminator: "")
+            
+            // Collect the input
+            guard let givenInput = readLine() else {
+                continue
+            }
+            
+            // If a list of acceptable values was provided...
+            if let allowed = acceptableValues {
+                
+                // ... then check that the given input is contained in the array of allowed values.
+                guard allowed.contains(givenInput) else {
+                    continue
+                }
+                
+            }
+            
+            // If we've made it past all the checks, the input is part of the list of allowed values.
+            // So, return it.
+            return givenInput
+            
+        }
+        
+    }
+    
+}
