0fbc3b236637c6637b17561e7a28b81bffa770b0
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-22 19:35:44 -0500

    created a file for RSANumbers

---
+++ iOS-RSANumbers/iOS_RSANumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_RSANumbersApp.swift
+//  iOS-RSANumbers
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_RSANumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
